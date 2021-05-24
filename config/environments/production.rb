85ad3b753407ed420ae560ee10416576b5dd003a
Matthieu HERNANDEZ <matthieu.hernandez1994@gmail.com>
2021-05-24 22:48:20 +0200

    Fix the bug

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,7 +66,7 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'talking-papers.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.talkingpapers.org' }
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.gmail.com',
     :port           => 587,
