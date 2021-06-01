d4ccbaffa268e78f301a6d6aa3adac0879a16c81
Matthieu HERNANDEZ <matthieu.hernandez1994@gmail.com>
2021-06-02 00:24:54 +0200

    Add param to switch tab automatically

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,137 +1,123 @@
 class ArticlesController < ApplicationController
-	require 'nokogiri'
-	require 'open-uri'
-	require 'uri'
+    require 'nokogiri'
+    require 'open-uri'
+    require 'uri'
 
-	def new
-		render '/static_pages/home'
-	end
+    def new
+        render '/static_pages/home'
+    end
 
-	def create
+    def create
+        #puts "---------- #{params[:active_tab]} ----------"
+        #puts "URL = #{parse_arxiv_url(article_params[:url])}"
 
-		if article_params[:is_arxiv] == "true"
-	  		@article = Article.new(:url => article_params[:url], :title => article_params[:title], :download_link => article_params[:download_link])
-	 	else
-	 		@article = Article.new(:url => article_params[:url], :title => article_params[:title], :download_link => article_params[:url])
-	 	end
+        @article = Article.where(:url => @article.url).first
+        if exist.nil?
+            if is_valid_arxiv_url(article_params[:url])
+                create_arxiv_article
+            elsif valid_non_arxiv_paper(article_params[:url])
+                if params[:active_tab] == 'others'
+                    create_non_arxiv_article
+                else
+                    redirect_to root_path (active_tab: 'others')
+                end
+            end
+        end
+        redirect_to @article
+    end
 
+    def show
+        @article = Article.find(params[:id])
+        if current_user.nil?
+            @notes = nil
+        else
+            @notes = @article.notes.where(user_id: current_user.id, is_public: false)\
+                        .order(:page_num)
+        end
 
-		 # if not existing, create. else, just redirect
-		existing = Article.where(:url => @article.url).first
-		if existing.nil?
-		  	  	
-		  	  	if article_params[:is_arxiv] == "true"
-		  	  	  	begin
-			  	  	  	if @article.url.include? "arxiv.org/abs/" 
-							  doc = Nokogiri::HTML(open(@article.url))
-							  title = doc.xpath('/html/head/meta[@name="citation_title"]/@content').to_s
-							  download_link = doc.xpath('/html/head/meta[@name="citation_pdf_url"]/@content').to_s
+        # @public_notes = @article.notes.where(is_public: true)\
+        #                 .order(:page_num)
+        # think need to modify this (find_by_sql) to include array of comments for each public note?
 
+        @public_notes = Note.find_by_sql(['''
+SELECT
+    notes.id,
+    notes.article_id,
+    notes.page_num,
+    notes.note_text,
+    notes.note_type,
+    notes.user_id,
+    notes.is_public,
+    notes.username,
+    notes.is_anon,
+    array_agg(comments.comment_text order by comments.created_at) comments_texts
+FROM
+    notes
+    INNER JOIN articles
+        ON articles.id = notes.article_id
+    LEFT JOIN comments
+        ON notes.id = comments.note_id
+WHERE
+    articles.id = ?
+    and notes.is_public
+GROUP BY
+    1,2,3,4,5,6,7,8,9
+ORDER BY
+    page_num''', params[:id]])
 
-							  @article.update(title: title, download_link: download_link)
+        @query = params[:query]
+    end
 
-							  @article.save
-							  redirect_to @article
-
-						else
-							flash[:danger] = 'Couldn\'t retrieve paper! Please check the arxiv URL (provide the abs page, not pdf link) and try again.'
-
-						  	redirect_to root_path
-						end
-
-					rescue 
-						flash[:danger] = 'Couldn\'t retrieve paper! Please check the arxiv URL (provide the abs page, not pdf link) and try again.'
-						  
-						redirect_to root_path
-					end
-
-				else
-					if valid_non_arxiv_paper?(@article.url)
-
-						if article_params[:title].delete(' ') != ""
-							@article.save
-							redirect_to @article
-						else
-							flash[:danger] = 'You must fill in the title of the article.'
-
-							redirect_to root_path
-						end
-					else
-						flash[:danger] = 'Invalid PDF link, please check and try again.'
-
-						redirect_to root_path
-					end
-				end
-
-
-		else
-			redirect_to existing
-		end
-
-	end
-
-	def show
-		@article = Article.find(params[:id])
-		if current_user.nil?
-			@notes = nil
-		else
-			@notes = @article.notes.where(user_id: current_user.id, is_public: false)\
-						.order(:page_num)
-		end
-
-		# @public_notes = @article.notes.where(is_public: true)\
-		# 				.order(:page_num)
-		# think need to modify this (find_by_sql) to include array of comments for each public note?
-
-		@public_notes = Note.find_by_sql(['''
-			select
-				notes.id,
-				notes.article_id,
-				notes.page_num,
-				notes.note_text,
-				notes.note_type,
-				notes.user_id,
-				notes.is_public,
-				notes.username,
-				notes.is_anon,
-				array_agg(comments.comment_text order by comments.created_at) comments_texts
-			from
-				notes
-					inner join articles
-						on (articles.id = notes.article_id)
-					left join comments
-						on (notes.id = comments.note_id)
-			where
-				articles.id = ?
-				and notes.is_public
-			group by
-				1,2,3,4,5,6,7,8,9
-			order by
-				page_num
-		''', params[:id]])
-
-		@query = params[:query]
-	end
- 
 private
-  def article_params
-    params.require(:article).permit(:url, :title, :download_link, :is_arxiv)
-  end
 
-  def valid_non_arxiv_paper?(uri)
-	  begin
-		url = URI.parse(uri)
-		req = Net::HTTP.new(url.host, url.port)
-		req.use_ssl = true
-		res = req.request_head(url.path)
-	    if res.code == "200" && res.content_type == "application/pdf"
-	    	true
-	    else
-	    	false
-	    end
-	  rescue
-	    false
-	  end
-  end
+    def article_params
+        params.require(:article).permit(:url, :title, :download_link, :is_arxiv)
+    end
+
+    def is_valid_arxiv_url(url)
+        begin
+            if url.include? "arxiv.org/abs/"
+                doc = Nokogiri::HTML(open(@article.url))
+                title = doc.xpath('/html/head/meta[@name="citation_title"]/@content').to_s
+                download_link = doc.xpath('/html/head/meta[@name="citation_pdf_url"]/@content').to_s
+                return url
+            end
+        rescue
+            flash[:danger] = 'Invalid arxiv link, please check the arxiv URL and try again. (provide the link of abs page instead of the pdf if possible)'
+            redirect_to root_path
+        end
+        return nil
+    end
+
+    def is_valid_non_arxiv_url(uri)
+        begin
+            url = URI.parse(uri)
+            req = Net::HTTP.new(url.host, url.port)
+            req.use_ssl = true
+            res = req.request_head(url.path)
+            if res.code == "200" && res.content_type == "application/pdf"
+                article_params[:download_link] = uri
+                true
+            else
+                false
+            end
+        rescue
+            flash[:danger] = 'Invalid pdf link, please check the URL and try again.'
+            redirect_to root_path
+        end
+    end
+
+    def create_arxiv_article(url)
+        title
+        @article = Article.new(:url => article_params[:url], :title => article_params[:title], :download_link => article_params[:download_link])
+        #@article.save
+    end
+
+    def create_non_arxiv_article(url)
+        if title
+        flash[:danger] = 'You must fill in the title of the article.'
+        @article = Article.new(:url => article_params[:url], :title => article_params[:title], :download_link => article_params[:download_link])
+        #@article.save
+    end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,30 +1,33 @@
 module ApplicationHelper
-	
+
 require 'kramdown'
 
-
-	def kramdown(text)
-		return Kramdown::Document.new(text).to_html
-	end
-
-
-  # Returns the full title on a per-page basis.       # Documentation comment
-  def full_title(page_title = '')                     # Method def, optional arg
-    base_title = "TalkingPapers"  # Variable assignment
-    if page_title.empty?                              # Boolean test
-      base_title                                      # Implicit return
-    else
-      page_title + " | " + base_title                 # String concatenation
+    def kramdown(text)
+        return Kramdown::Document.new(text).to_html
     end
-  end
 
-  def will_paginate(collection_or_options = nil, options = {})
-    if collection_or_options.is_a? Hash
-      options, collection_or_options = collection_or_options, nil
+    # Change the active tab.
+    def is_active(*links)
+        links.each { |link| return 'active' if params[:active_tab] == link }
     end
-    unless options[:renderer]
-      options = options.merge renderer: WillPaginate::ActionView::BootstrapLinkRenderer
+
+  # Returns the full title on a per-page basis.  # Documentation comment
+    def full_title(page_title = '')              # Method def, optional arg
+        base_title = "TalkingPapers"             # Variable assignment
+        if page_title.empty?                     # Boolean test
+            base_title                           # Implicit return
+        else
+            page_title + " | " + base_title      # String concatenation
+        end
     end
-    super *[collection_or_options, options].compact
-  end
- end
+
+    def will_paginate(collection_or_options = nil, options = {})
+        if collection_or_options.is_a? Hash
+            options, collection_or_options = collection_or_options, nil
+        end
+        unless options[:renderer]
+            options = options.merge renderer: WillPaginate::ActionView::BootstrapLinkRenderer
+        end
+        super *[collection_or_options, options].compact
+    end
+end
