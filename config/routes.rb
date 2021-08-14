7458c0a597670542e64a9c6507f8f7c22b4ae891
Matthieu HERNANDEZ <matthieu.hernandez1994@gmail.com>
2021-08-14 15:55:09 +0200

    Add privacy policy

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   root   'static_pages#home'
   get    '/help',    to: 'static_pages#help'
   get    '/about',   to: 'static_pages#about'
+  get    '/privacy_policy',   to: 'static_pages#privacy_policy'
   get    '/contact', to: 'static_pages#contact'
   get    '/signup',  to: 'users#new'
   get    '/connect', to: 'users#get_or_create_from_provider'
